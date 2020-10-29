b8aa54d2dd0d9eb4ab95feb6a89b6a259bf94d57
richardwan <richard.f.wan@gmail.com>
2020-10-29 09:26:34 -0600

    hex grid

--- src/component/HelloWorldStringComponent.js
+++ src/component/HelloWorldStringComponent.js
@@ -1,38 +1,13 @@
 import React, { Component } from 'react';
 import HexTileComponent from './HexTileComponent'
+import HexGridComponent from "./HexTileComponent";
 
 class HelloWorldStringComponent extends Component {
     render() {
         return (<>
-          <table className="hexgrid">
-              <tbody>
-                <tr>
-                    <td><HexTileComponent>ab cd ef gh jk lm no</HexTileComponent></td>
-                    <td><HexTileComponent>b</HexTileComponent></td>
-                    <td><HexTileComponent>c</HexTileComponent></td>
-                    <td><HexTileComponent>d</HexTileComponent></td>
-                </tr>
-                <tr>
-                    <td><HexTileComponent>eFgh ijklm nop q rus tuvxy</HexTileComponent></td>
-                    <td><HexTileComponent>f</HexTileComponent></td>
-                    <td><HexTileComponent>g</HexTileComponent></td>
-                    <td><HexTileComponent>h</HexTileComponent></td>
-                </tr>
-                <tr>
-                  <td><HexTileComponent>i</HexTileComponent></td>
-                  <td><HexTileComponent>j</HexTileComponent></td>
-                  <td><HexTileComponent>k</HexTileComponent></td>
-                  <td><HexTileComponent>l</HexTileComponent></td>
-                </tr>
-                <tr>
-                  <td><HexTileComponent>m</HexTileComponent></td>
-                  <td><HexTileComponent>n</HexTileComponent></td>
-                  <td><HexTileComponent>o</HexTileComponent></td>
-                  <td><HexTileComponent>p</HexTileComponent></td>
-                </tr>
-              </tbody>
+          <HexGridComponent size="7">
 
-          </table>
+          </HexGridComponent>
             <HexTileComponent>
                 FOO
             </HexTileComponent>
--- src/component/HexTileComponent.js
+++ src/component/HexTileComponent.js
@@ -1,16 +1,46 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-class HexTileComponent extends Component {
+class HexGridComponent extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      size: parseInt(props.size)
+    }
+  }
+
   render() {
-    return (
-        <div className="hex">
-          <div className="hex-background">
-            &#x2B22;
-          </div>
-          <div className="hex-content">{this.props.children}</div>
-        </div>
+    return (<div>
+      <table className="hexgrid">
+        <tbody>
+        {this.indices().map(i => this.row(i))}
+        </tbody>
+      </table>
+      </div>
     )
+  }
+
+  row(i) {
+    let key = "row"+i
+    return <tr key={key}>{this.indices().map(j => this.tile(i, j))}</tr>
+  }
+
+  indices() {
+    let size = this.state.size;
+    debugger
+    return Array(size || 0).fill().map((v, i) => i)
+  }
+
+  tile(row, column) {
+    let key = "cell" + row + ":" + column
+    return <td key={key}>
+      <div className="hex">
+        <div className="hex-background">
+          &#x2B22;
+        </div>
+        <div className="hex-content">{key}</div>
+      </div>
+    </td>
   }
 }
 
-export default HexTileComponent
+export default HexGridComponent
