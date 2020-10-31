7a93d97073747393cd3b8b46136b34aa4f71cafa
richardwan <richard.f.wan@gmail.com>
2020-10-31 15:00:38 -0600

    remove http calls

--- src/component/HelloWorldStringComponent.test.js
+++ src/component/HelloWorldStringComponent.test.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {setupServer} from 'msw/node'
-import {rest} from 'msw'
-import { waitFor } from '@testing-library/react';
 
 import SUT from './HelloWorldStringComponent';
 
@@ -14,25 +11,11 @@ beforeEach(() => {
     document.body.appendChild(container);
 });
 
-const server = setupServer(
-    rest.get('http://localhost:8080/hello-world', (req, res, ctx) => {
-        return res(ctx.json('hello there'))
-    }),
-)
-
 afterEach(() => {
     document.body.removeChild(container);
     container = null;
-    server.resetHandlers()
 });
-
-beforeAll(() => server.listen())
-afterAll(() => server.close())
 
 it('has a welcome message', async () => {
     expect(container.querySelector('.jt_message').textContent).toBe("initial message")
-    await waitFor(() => {
-        let textContent = container.querySelector('.jt_message').textContent;
-        expect(textContent).toBe("initial message")
-    })
 });
